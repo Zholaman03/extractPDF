22c56f39ab4c3a1438261e0669adf9742bb92885
Zholaman03 <110783570+Zholaman03@users.noreply.github.com>
2025-07-11 18:00:33 +0600

    Update settings.py

--- extract_pdf/settings.py
+++ extract_pdf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3=^j)^ic8rm_#n=fyty%oj@b8fmkp2+bfe793oy^f3#ew__naj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['extractpdf-txjo.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
