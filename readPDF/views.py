a4f5c1d4368cbbf841589216529e9104310b613c
Zholaman03 <110783570+Zholaman03@users.noreply.github.com>
2025-07-26 23:19:17 +0600

    updated

--- readPDF/views.py
+++ readPDF/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import PyPDF2
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
